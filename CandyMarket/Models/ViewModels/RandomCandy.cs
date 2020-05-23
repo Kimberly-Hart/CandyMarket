e2c0f2600a6b46a2a8a60bff564e7d8f7e939a20
Raymond <raymond.techcoder@gmail.com>
2020-05-23 12:17:54 -0500

    WIP

---
+++ CandyMarket/Models/ViewModels/RandomCandy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyMarket.Models.ViewModels
+{
+    public class RandomCandy
+    {
+    }
+}
